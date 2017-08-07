9f55ac3b5a4ec4074b6a5d290c32c9be44af549e
Magnus Fahlin <magnus@fahlin.org>
2017-08-07 21:01:58 +0200

    Attaches JWT header to all requests

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -5,9 +5,16 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { browserHistory, Router } from 'react-router';
 import routes from './routes.js';
+import Auth from './modules/Auth.js';
 
+const axios = require('axios')
 // remove tap delay, essential for MaterialUI to work properly
 injectTapEventPlugin();
+
+axios.interceptors.request.use(request => {
+  axios.defaults.headers.common['Authorization'] = 'bearer ' + Auth.getToken();
+  return request;
+});
 
 ReactDom.render((
   <MuiThemeProvider muiTheme={getMuiTheme()}>
