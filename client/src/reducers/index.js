53363f861d56f91fe1ac111fc6d59af1e624ae5f
Magnus Fahlin <magnus@fahlin.org>
2017-12-23 20:26:51 +0100

    Fixed problem with posts

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux';
 import authentication from './authentication';
 import menu from './menu';
-import postViewer from './postViewer';
 import posts from './posts';
 const rootReducer = combineReducers({
   authentication,
   posts,
-  postViewer,
   menu
 })
 
